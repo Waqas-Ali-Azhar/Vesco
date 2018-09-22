b5fcef7736d2d563f868b68d3419cc4512fa5d89
Waqas Ali <waqas_111@hotmail.com>
2018-09-22 20:32:02 +0500

    built work and team section

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,31 @@ $(function(){
 	// animate on scroll
 	new WOW().init();
 });
+
+
+/* =======================================================
+						WORK
+======================================================= */
+
+$(function(){
+	$('#work').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		gallery:{
+			enabled:true,
+		}
+		});
+});
+
+/* =======================================================
+						TEAM
+======================================================= */
+$(function(){
+	$('#team-members').owlCarousel({
+		items: 3,
+		autoplay : true,
+		smartSpeed: 700,
+		loop: true,
+	});
+});
+
